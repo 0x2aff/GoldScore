672ec8d12266789655ab9fef84f9de53e056422e
0x2aff <schlosser.st@gmail.com>
2018-04-07 15:14:04 +0200

    Fix some bugs and add newest version (v.0.0.3).

--- GoldScore/Properties/AssemblyInfo.cs
+++ GoldScore/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Windows;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("GoldScore")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("TSM Group Maker")]
+[assembly: AssemblyConfiguration("Retail")]
+[assembly: AssemblyCompany("exceptionptr")]
 [assembly: AssemblyProduct("GoldScore")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -49,5 +49,5 @@ using System.Windows;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.3")]
+[assembly: AssemblyFileVersion("0.0.3")]
